c48e937ffbfd0383712244bf1eed821e4e714541
SedeSimeon <SedeSimeon>
2023-02-23 03:52:20 -0800

    4

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -21,13 +21,14 @@ int main(void)
 					putchar(l);
 					if (n != 55 || m != 56)
 					{
-						putchar(' ');
-						putchar(' ');
+						putchar(',');
+						putcher(' ');
 					}
-				}	
+				}
 			}
 		}
 	}
 	putchar('\n');
 	return (0);
 }
+
